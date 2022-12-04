b86c8edc361bfefd2afdd39e0582f3cebaee301d
marvin-j97 <marvin.janke.97@gmail.com>
2022-12-05 00:22:35 +0100

    chore: setup repo

---
+++ packages/eslint-config-custom/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: ["turbo", "prettier", "plugin:import/recommended", "plugin:import/typescript"],
+  plugins: ["simple-import-sort"],
+  rules: {
+    "camelcase": "error",
+    "yoda": "error",
+    "eqeqeq": "error",
+
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "import/newline-after-import": "error",
+  },
+};
