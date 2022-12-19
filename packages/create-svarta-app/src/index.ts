0ab05a37fee10405902c7da819cd0c4880795b0a
marvin-j97 <marvin.janke.97@gmail.com>
2022-12-19 02:24:46 +0100

    make create-app nicer

--- packages/create-svarta-app/src/index.ts
+++ packages/create-svarta-app/src/index.ts
@@ -1,36 +1,67 @@
 import chalk from "chalk";
+import { detect as detectManager, PM } from "detect-package-manager";
+import { existsSync } from "fs";
 import { downloadTemplate } from "giget";
 import prompts from "prompts";
 
+import { getInstallCommand, getRunCommand } from "./npm_client";
+
 type Template = "starter-http";
 
-async function cloneTemplate(appDir: string, template: Template): Promise<void> {
-  console.log(`Cloning ${chalk.blueBright(template)} into ${chalk.blueBright(appDir)}...`);
-  await downloadTemplate(`github:marvin-j97/svarta/templates/${template}`, {
+async function cloneTemplate(appDir: string, template: Template, npmClient: PM): Promise<void> {
+  console.log(`\nCloning ${chalk.blueBright(template)} into ${chalk.blueBright(appDir)}...`);
+  await downloadTemplate(`gh:marvin-j97/svarta/templates/${template}`, {
     dir: appDir,
+    preferOffline: false,
   });
 
-  console.log(`\nNext, run: `);
-  console.log(chalk.blueBright(`  cd ${appDir}\n`));
-
-  console.log(chalk.blueBright(`  npm i`));
-  console.log(chalk.blueBright(`  yarn`));
-  console.log(chalk.blueBright(`  pnpm i`));
-
-  console.log(`\nTo start the development server, run:`);
-  console.log(chalk.blueBright(`  npm run dev`));
-  console.log(chalk.blueBright(`  yarn dev`));
-  console.log(chalk.blueBright(`  pnpm dev`));
+  console.log(`\nNext, run:`);
+  console.log(`  ${chalk.grey("1:")} ${chalk.blueBright(`cd ${appDir}`)}`);
+  console.log(`  ${chalk.grey("2:")} ${chalk.blueBright(getInstallCommand(npmClient))}`);
+  console.log(`  ${chalk.grey("3:")} ${chalk.blueBright(getRunCommand(npmClient, "dev"))}`);
 }
 
+const managers = [
+  {
+    value: "npm",
+    title: "npm",
+    color: chalk.red,
+  },
+  {
+    value: "pnpm",
+    title: "pnpm",
+    color: chalk.yellowBright,
+  },
+  {
+    value: "yarn",
+    title: "yarn",
+    color: chalk.blue,
+  },
+];
+
 (async () => {
-  const { appDir, template } = await prompts([
+  console.log("create-svarta-app 0.0.0");
+
+  const manager = await detectManager();
+  const managerIndex = managers.findIndex(({ value }) => value === manager);
+  const managerObject = managers.find(({ value }) => value === manager) ?? managers[0];
+
+  console.log(`Detected package manager: ${managerObject?.color(manager)}\n`);
+
+  const { appDir, template, chosenManager } = await prompts([
     {
       name: "appDir",
       type: "text",
       message: "Enter project folder",
       hint: "my-app",
       initial: "my-app",
+      validate: (appDir: string) => {
+        if (existsSync(appDir)) {
+          console.error(chalk.red(`\n\nFolder ${appDir} already exists! Aborting...`));
+          process.exit(1);
+        }
+        return true;
+      },
     },
     {
       name: "template",
@@ -45,14 +76,21 @@ async function cloneTemplate(appDir: string, template: Template): Promise<void> 
       ],
       initial: 0,
     },
+    {
+      name: "chosenManager",
+      type: "select",
+      message: "Choose package manager",
+      choices: managers,
+      initial: Math.max(managerIndex, 0),
+    },
   ]);
 
   if (!appDir || !template) {
     console.log("Aborting...");
-    process.exit(0);
+    process.exit(1);
   }
 
-  await cloneTemplate(appDir, template);
+  await cloneTemplate(appDir, template, chosenManager);
 
   process.exit(0);
 })();
---
+++ packages/create-svarta-app/src/npm_client.ts
@@ -0,0 +1,27 @@
+import type { PM } from "detect-package-manager";
+
+const installCommands: Record<PM, string> = {
+  npm: `npm i`,
+  pnpm: `pnpm i`,
+  yarn: `yarn`,
+};
+
+const runCommands: Record<PM, (client: string) => string> = {
+  npm: (cmd) => `npm run ${cmd}`,
+  yarn: (cmd) => `yarn ${cmd}`,
+  pnpm: (cmd) => `pnpm run ${cmd}`,
+};
+
+/**
+ * Gets the install command of the chosen NPM client
+ */
+export function getInstallCommand(client: PM): string {
+  return installCommands[client];
+}
+
+/**
+ * Gets the run command of the chosen NPM client
+ */
+export function getRunCommand(client: PM, cmd: string): string {
+  return runCommands[client](cmd);
+}
