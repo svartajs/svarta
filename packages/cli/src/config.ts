381788209e3cc466fdeeff5f52bc829289ab25a2
marvin-j97 <marvin.janke.97@gmail.com>
2022-12-20 01:01:25 +0100

    fix load config

--- packages/cli/src/config.ts
+++ packages/cli/src/config.ts
@@ -1,12 +1,14 @@
+import { resolve } from "node:path";
+
 import { Config, configSchema } from "@svarta/core";
 import chalk from "chalk";
 
 // TODO: move into CORE, don't throw, but result
 
 export async function loadConfig(path: string): Promise<Config> {
-  console.log(`[@svarta/cli] Loading config ${chalk.yellow(path)}\n`);
+  console.log(`[@svarta/cli] Loading config ${chalk.yellow(path)}`);
 
-  let config = await import(path);
+  let config = await import(resolve(path));
   if (config.default) {
     config = config.default;
   }
