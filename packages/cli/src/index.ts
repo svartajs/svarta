b5525b16d78a9b145c344d5552d029bb30323792
marvin-j97 <marvin.janke.97@gmail.com>
2022-12-20 01:01:40 +0100

    cli init command

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,4 +1,5 @@
 import chalk from "chalk";
+import { execSync } from "child_process";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
@@ -8,11 +9,30 @@ import { loadConfig } from "./config";
 
 const DEFAULT_CONFIG_PATH = "svarta.config.mjs";
 
+const createCommands: Record<string, (client: string) => string> = {
+  npm: (cmd) => `npm create ${cmd}`,
+  yarn: (cmd) => `yarn create ${cmd}`,
+  pnpm: (cmd) => `pnpm create ${cmd}`,
+};
+
 yargs(hideBin(process.argv))
   .scriptName("svarta")
   .version("0.0.0")
-  /* TODO: init */
   /* TODO: deploy */
+  .command(
+    "init <path>",
+    "Scaffold new project",
+    (yargs) =>
+      yargs.positional("path", { type: "string" }).option("manager", {
+        choices: ["npm", "yarn", "pnpm"],
+        default: "npm",
+      }),
+    async (args) => {
+      execSync(createCommands[args.manager]("svarta-app"), {
+        stdio: "inherit",
+      });
+    },
+  )
   .command(
     "build",
     "Build server",
@@ -26,9 +46,9 @@ yargs(hideBin(process.argv))
         },
       }),
     async (argv) => {
-      console.log(`[@svarta/cli] Starting build\n`);
+      console.log(`[@svarta/cli] Starting build`);
       const config = await loadConfig(argv.config);
-      console.log(`[@svarta/cli] Using adapter ${chalk.blueBright(`"${config.adapter.type}"`)}\n`);
+      console.log(`[@svarta/cli] Using adapter ${chalk.blueBright(`"${config.adapter.type}"`)}`);
       await build(config);
     },
   )
@@ -45,7 +65,7 @@ yargs(hideBin(process.argv))
         },
       }),
     async (argv) => {
-      console.log(`[@svarta/cli] Starting development server\n`);
+      console.log(`[@svarta/cli] Starting development server`);
       const config = await loadConfig(argv.config);
       await startDevelopmentServer(config);
     },
